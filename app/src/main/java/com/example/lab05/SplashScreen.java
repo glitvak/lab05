c52b60ad90afe37b9fcecb47dd9f0b8273e38483
Gil Litvak <43884803+glitvak@users.noreply.github.com>
2020-10-11 20:53:02 -0400

    added audio file functionality

--- app/src/main/java/com/example/lab05/SplashScreen.java
+++ app/src/main/java/com/example/lab05/SplashScreen.java
@@ -3,16 +3,20 @@ package com.example.lab05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 5000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(SplashScreen.this, R.raw.doorbell);
+        mediaPlayer.start();
 
         new Handler().postDelayed(new Runnable() {
 
